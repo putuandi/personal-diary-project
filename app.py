9447d213490baf4966f18c3f0476a9476087335e
putuandi <fergusoboy123@gmail.com>
2024-03-31 00:15:39 +0800

    update

--- app.py
+++ app.py
@@ -12,5 +12,11 @@ def show_diary():
     print(sample_receive)
     return jsonify({'message': 'data received all good!!!'})
 
+@app.route('/diary', methods=['POST'])
+def add_to_diary():
+    sample_receive = request.form.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': 'data was saved!'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
